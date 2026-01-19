b49f7228348ca059f76975448978339845922aea
SebaGarea <sebastiangarea@outlook.com>
2026-01-19 12:42:05 -0300

    se agrega tipos a proteccion

--- src/components/ItemAddRawMaterials/ItemAddRawMaterials.jsx
+++ src/components/ItemAddRawMaterials/ItemAddRawMaterials.jsx
@@ -85,7 +85,8 @@ export const ItemAddRawMaterials = ({ RawMaterialId }) => {
       "Tiradores",
       "Cerraduras",
       "Tornillos",
-      "Tirafondos"
+      "Tirafondos",
+      "Otros",
     ],
     Otros: [
     ]
